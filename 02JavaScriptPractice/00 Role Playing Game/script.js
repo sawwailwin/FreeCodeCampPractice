20d41a40ce44b67a12a12ac41b84d3d7f8c806e8
sawwailwin <sawwailwin.2022@gmail.com>
2025-07-21 00:00:15 +0630

    updated goFight function

--- 02JavaScriptPractice/00 Role Playing Game/script.js
+++ 02JavaScriptPractice/00 Role Playing Game/script.js
@@ -152,9 +152,13 @@ function fightDragon() {
 }
 
 function goFight() {
+  function goFight() {
   update(locations[3]);
   monsterHealth = monsters[fighting].health;
   monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
+}
 
 }
 
